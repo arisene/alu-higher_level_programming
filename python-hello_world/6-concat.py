cbe9407e1a658f1d392e59a7e77ab38afbb37883
arisene <arisenee@pop-os.localdomain>
2022-10-28 06:58:33 +0200

    task 4-7

---
+++ python-hello_world/6-concat.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# print out the variables
+str1 = "Holberton"
+str2 = "School"
+print(f"Welcome to {str1} {str2}!")
---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+# print the first 3 letters
+print(f"First 3 letters: word[0:2]")
+#print the last 2 letters 
+print(f"Last 2 letters: word[6:]")
+#print the middle letter
+print(f"Middle word: word[-5]")
