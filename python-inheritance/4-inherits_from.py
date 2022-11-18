547ca58ca55211b7137fcd343a807a2444d1e668
arisene <arisenee@pop-os.localdomain>
2022-11-18 22:27:51 +0200

    update

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -12,4 +12,4 @@ def inherits_from(obj, a_class):
         True: if the object is an instance
         False: if the object isn't an instance
     '''
-    return isinstance(obj, a_class) and type(obj) != 
+    return isinstance(obj, a_class) and type(obj) != a_class 
